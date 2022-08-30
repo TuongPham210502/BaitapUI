c5cb6f90360e5e3b1f1bfa1191efb95f44a8c006
= <=>
2022-08-30 21:45:18 +0700

    add họ tên MSV & event button Login

--- src/main/java/com/example/firstapp/MainActivity.java
+++ src/main/java/com/example/firstapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProfile();
+            }
+        });
+    }
+
+    public void openProfile(){
+        Intent intent = new Intent(this,Profile.class);
+        startActivity(intent);
     }
 }
